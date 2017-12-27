5ad0b5ef8e588d49c021cffb0383126f5d9e26c8
admin <admin@admins-mac-mini.local>
2017-12-27 10:45:17 +0200

    Add prepare

--- Jarvis/ViewController.swift
+++ Jarvis/ViewController.swift
@@ -46,6 +46,10 @@ class ViewController: UIViewController {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
